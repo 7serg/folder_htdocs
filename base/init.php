9a1f94600447819627ce170da5ed5d0a7b092e43
Sergey Malko <Sergey Malko>
2016-03-15 00:14:10 +0200

    file model has been created

---
+++ base/init.php
@@ -0,0 +1,12 @@
+<?php  
+session_start();
+const TPL_DIR = 'tpl/';
+const LGC_DIR = 'logic/';
+const MODEL_DIR = 'model/';
+
+require 'base/helpers.php';
+
+require MODEL_DIR . 'model.php';
+
+
+?>
--- index.php
+++ index.php
@@ -2,20 +2,24 @@
 // preg_match_all('/\/(\w+)/', $_SERVER['REQUEST_URI'], $parts);
 //$parts = $parts[1];
 
-include 'tpl/header.html';
+require 'base/init.php';
+//const LGC_DIR = 'logic/';
+//const TPL_DIR = 'tpl/';
+
+include TPL_DIR . 'header.html';
 
 switch (TRUE) {
 	case preg_match('/^\/(|home)$/', $_SERVER['REQUEST_URI']):
-		require 'logic/home.php';
+		require LGC_DIR . 'home.php';
 		break;
 	case preg_match('/^\/user$/', $_SERVER['REQUEST_URI']):
-		require 'logic/user/user.php';
+		require LGC_DIR . 'user/user.php';
 		break;
 	default:
-		require 'logic/404.php';
+		require LGC_DIR .'404.php';
 		break;
 }
 
 
-include 'tpl/footer.html';
+include TPL_DIR . 'footer.html';
 ?>
